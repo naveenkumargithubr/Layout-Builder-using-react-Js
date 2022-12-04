2a38707272c2d5bf4a2ca7f9636214a491aebdc0
naveen kumar <112088541+naveenkumargithubr@users.noreply.github.com>
2022-12-04 19:38:29 +0530

    configuarationcontroll

--- src/components/ConfigurationController/index.js
+++ src/components/ConfigurationController/index.js
@@ -1,9 +1,11 @@
 // Write your code here
-import ConfigurationContext from '../../context/ConfigurationContext'
+import ConfigurationContext from '../../context/ConfigurationContext' // first we import the React Context when we create the context component
 
 import './index.css'
 
 const ConfigurationController = () => (
+  
+  // step1 here we can access the values from the react context 
   <ConfigurationContext.Consumer>
     {value => {
       const {
@@ -15,14 +17,20 @@ const ConfigurationController = () => (
         onToggleShowRightNavbar,
       } = value
 
+  
+  // in this method we updating the content eighter true / false if it is true it will display the content
       const onChangeContent = event => {
         onToggleShowContent(event.target.value)
       }
 
+  // same process here also we updating the leftnavbar 
+      
       const onChangeLeftNavbar = event => {
         onToggleShowLeftNavbar(event.target.value)
       }
 
+  // same process here also we updating the rightnavbar
+      
       const onChangeRightNavbar = event => {
         onToggleShowRightNavbar(event.target.value)
       }
@@ -35,8 +43,8 @@ const ConfigurationController = () => (
               <input
                 type="checkbox"
                 id="content"
-                checked={showContent}
-                onChange={onChangeContent}
+                checked={showContent}  // this method shows the data when it is true
+                onChange={onChangeContent} // step 2 updating the checkbox content
               />
               <label htmlFor="content" className="content-heading">
                 Content
@@ -47,8 +55,8 @@ const ConfigurationController = () => (
               <input
                 type="checkbox"
                 id="leftnavbar"
-                checked={showLeftNavbar}
-                onChange={onChangeLeftNavbar}
+                checked={showLeftNavbar} // this method shows the data when it is true
+                onChange={onChangeLeftNavbar} // step 2 updating the checkbox content
               />
               <label htmlFor="leftnavbar" className="content-heading">
                 Left Navbar
@@ -59,8 +67,8 @@ const ConfigurationController = () => (
               <input
                 type="checkbox"
                 id="rightnavbar"
-                checked={showRightNavbar}
-                onChange={onChangeRightNavbar}
+                checked={showRightNavbar} // this method shows the data when it is true
+                onChange={onChangeRightNavbar}  // step 2 updating the checkbox content
               />
               <label htmlFor="rightnavbar" className="content-heading">
                 Right Navbar
